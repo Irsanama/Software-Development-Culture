8cf1cb08bec3a533ad7d37deec742001b3c1ae48
Irsanama1 <pavel_bogomazov1@inbox.ru>
2025-02-04 00:50:49 +0800

    Add /items/{item_id} endpoint

--- Task 1. git-branches/main.py
+++ Task 1. git-branches/main.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app = FastAPI()
+
+items = [
+    {"id": 1, "name": "item1"},
+    {"id": 2, "name": "item2"},
+    {"id": 3, "name": "item3"}
+]
 
 @app.get("/")
 def read_root():
@@ -9,3 +15,10 @@ def read_root():
 @app.get("/items")
 def get_items():
     return {"items": ["item1", "item2", "item3"]}
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int = Path(...)):
+    for item in items:
+        if item["id"] == item_id:
+            return item
+    return {"Error": "Item not found"}
